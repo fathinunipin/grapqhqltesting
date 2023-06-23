fdc24d0e7d67abdbf6ee699d5c0822318553a629
fathin.ihsanudin <fathin.ihsanudin@unipin.com>
2023-06-23 08:50:15 +0700

    add apollo service

--- graphqltesting/src/main.ts
+++ graphqltesting/src/main.ts
@@ -1,6 +1,17 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { Apollo } from 'apollo-angular';
+   import { HttpLink } from 'apollo-angular-link-http';
+   import { InMemoryCache } from 'apollo-cache-inmemory';
 import { AppModule } from './app/app.module';
+
+const uri = 'https://your-graphql-api-url'; // Replace with your GraphQL API endpoint
+
+// export function createApollo(httpLink: HttpLink): Apollo {
+//   return new Apollo({
+//     link: httpLink.create({ uri }),
+//     cache: new InMemoryCache(),
+//   });
+// }
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
